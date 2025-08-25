d84a916e5db78bfc764bcb95d1a51b443ed289a8
Adham Basheir <adhambasheir000@gmail.com>
2025-08-25 14:16:53 +0300

    feat(config): dynamic DB host selection via APP_ENV

--- app/backend/initializers/db.go
+++ app/backend/initializers/db.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -12,9 +13,11 @@ import (
 var DB *gorm.DB
 
 func ConnectToDB() {
+	env := strings.ToUpper(os.Getenv("APP_ENV"))
+
 	dsn := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s channel_binding=%s",
-		os.Getenv("DB_HOST"),
+		os.Getenv("DB_HOST_"+env),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
