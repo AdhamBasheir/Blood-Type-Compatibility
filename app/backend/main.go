7f8cc150ace49b40af27c81b3f205f268798c5bb
Adham Basheir <adhambasheir000@gmail.com>
2025-09-09 21:39:03 +0300

    chore(main): add Logger and Recovery middleware to Gin router

--- app/backend/main.go
+++ app/backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"blood-type-compatibility/initializers"
+	middleware "blood-type-compatibility/middlewares"
 	"blood-type-compatibility/routes"
 
 	"github.com/gin-gonic/gin"
@@ -10,14 +11,19 @@ import (
 func init() {
 	initializers.InitEnv()
 	initializers.ConnectToDB()
+	initializers.InitLogger()
 }
 
 func main() {
-	router := gin.Default()
+	router := gin.New()
+	router.Use(
+		middleware.Recovery(),
+		middleware.Logger(),
+	)
 
 	routes.RegisterHealthRoutes(router)
 	routes.RegisterUserRoutes(router)
 	routes.RegisterBloodRoutes(router)
 
-	router.Run() // listen and serve on 0.0.0.0:8080
+	router.Run()
 }
