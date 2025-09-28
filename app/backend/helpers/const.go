940e09d6f6af5da10d1573088567a0599f27e9a3
Adham Basheir <adhambasheir000@gmail.com>
2025-09-29 00:14:32 +0300

    chore(helpers): add session, security, and user-related constants in const.go

--- app/backend/helpers/const.go
+++ app/backend/helpers/const.go
@@ -3,3 +3,18 @@ package helpers
 const (
 	TimeFormat = "2006-01-02 15-04-05 Mon"
 )
+
+// Session and security related constants
+const (
+	SessionDuration    = 3600 * 24 * 7 // 7 days
+	SessionTokenLength = 32
+	CSRFTokenLength    = 32
+)
+
+// User related constants
+const (
+	MaxLoginAttempts   = 5
+	LoginAttemptWindow = 15 // in minutes
+	PasswordMinLength  = 8
+	PasswordMaxLength  = 64
+)
