7e520f1530e6e4a4155172aa2d1201c00b557f5d
Adham Basheir <adhambasheir000@gmail.com>
2025-09-29 00:15:20 +0300

    feat(models): add session and CSRF token fields to User model

--- app/backend/models/user_model.go
+++ app/backend/models/user_model.go
@@ -4,9 +4,11 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Name        string    `gorm:"not null"`
-	Username    string    `gorm:"uniqueIndex;not null"`
-	Password    string    `gorm:"not null"`
-	BloodTypeID uint      `gorm:"not null"` // Foreign key
-	BloodType   BloodType `gorm:"constraint:OnUpdate:CASCADE,OnDelete:RESTRICT;"`
+	Name         string    `gorm:"not null"`
+	Username     string    `gorm:"uniqueIndex;not null"`
+	Password     string    `gorm:"not null"`
+	BloodTypeID  uint      `gorm:"not null"` // Foreign key
+	BloodType    BloodType `gorm:"constraint:OnUpdate:CASCADE,OnDelete:RESTRICT;"`
+	SessionToken string    `gorm:"uniqueIndex"`
+	CSRFToken    string    `gorm:"uniqueIndex"`
 }
